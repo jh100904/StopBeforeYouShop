326d94ca73ecc0d9907b0c7b5b8aa612dc8d41fe
MikeHughes-BIN <mike.hughes0506@gmail.com>
2026-06-23 16:52:26 +0200

    Update 23.06.2026 16:52:26,35

--- deploy.bat
+++ deploy.bat
@@ -1,36 +1,31 @@
 @echo off
 REM ====================================================
 REM  Stop Before You Shop - 1-Klick-Upload zu GitHub
-REM  Einfach doppelklicken. Fertig.
+REM  Raeumt automatisch Sperren + zu grosse Dateien auf.
 REM ====================================================
 cd /d "%~dp0"
 
 echo.
-echo === Aenderungen werden hochgeladen... ===
+echo === Aufraeumen (Sperren + zu grosse Videodatei) ===
+del ".git\index.lock"  2>nul
+del ".git\HEAD.lock"   2>nul
+git rm --cached --ignore-unmatch "klamotten-konsum.mp4" >nul 2>nul
+
 echo.
-
+echo === Aenderungen werden vorbereitet... ===
 git add -A
-
-REM Commit mit Datum+Uhrzeit als Beschreibung
 git commit -m "Update %date% %time%"
-
-if errorlevel 1 (
-  echo.
-  echo --- Keine neuen Aenderungen zum Hochladen gefunden. ---
-)
+if errorlevel 1 echo --- Nichts Neues zu committen (ok) ---
 
 echo.
 echo === Wird zu GitHub gepusht... ===
-echo.
-
 git push origin main
 
 echo.
 echo ============================================================
 echo  FERTIG!
-echo  Warte 1-2 Minuten, dann oeffne im Browser:
+echo  Warte 1-2 Minuten, dann im PRIVATEN Fenster oeffnen:
 echo  https://jh100904.github.io/StopBeforeYouShop/
-echo  und druecke STRG+F5
 echo ============================================================
 echo.
 pause
